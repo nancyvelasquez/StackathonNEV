434c8695713b172d686fdbfbb375e2bb4b4da075
nancyvelasquez <nevelasquez113@gmail.com>
2017-08-05 12:01:51 -0400

    modified secret

--- app/components/LoginSpotify.js
+++ app/components/LoginSpotify.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 
-const spotifyClientId = process.env.SPOTIFY_CLIENT_ID.replace(/"/g, '') 
-//|| require('../../secrets.js').spotifyClientId
-const spotifyRedirectURI = process.env.SPOTIFY_REDIRECT_URI.replace(/"/g, '') 
-//|| require('../../secrets.js').spotifyRedirectURI
+const spotifyClientId = process.env.SPOTIFY_CLIENT_ID.replace(/"/g, '') || require('../../secrets.js').spotifyClientId
+const spotifyRedirectURI = process.env.SPOTIFY_REDIRECT_URI.replace(/"/g, '') || require('../../secrets.js').spotifyRedirectURI
 
 export default class LoginSpotify extends Component {
   constructor() {
--- secret.js
+++ secret.js
@@ -1,6 +1,3 @@
 module.exports = {
-  spotifyClientId: 'YOUR CLIENT ID GOES HERE',
-  spotifyClientSecret: 'YOUR CLIENT SECRET GOES HERE',
-  spotifyRedirectURI: 'YOUR REDIRECT URI GOES HERE',
   googleKey: 'YOUR GOOGLE KEY GOES HERE'
 }
