a9c412badcb866a885233352d5e36616b6d29904
nancyvelasquez <nevelasquez113@gmail.com>
2017-08-05 11:05:26 -0400

    Testing

--- app/components/Browser.js
+++ app/components/Browser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM, { render } from 'react-dom';
 import { NavLink, Router, Route, IndexRedirect, browserHistory } from 'react-router-dom';
 import Viewer from './Viewer';
@@ -14,18 +14,113 @@ const examples = [
   },
 ];
 
-const Browser = ({ match }) => {
-  const { params } = match;
-  const activeExample = params.slug && examples.find(example => example.slug === params.slug);
-  return (
-    <div>
-      <Viewer example={activeExample} />
-    </div>
-  );
-};
+// const Browser = ({ match }) => {
+class Browser extends Component {
+  constructor(props) {
+    super(props) 
+    this.getHashParams = this.getHashParams.bind(this)
+  }
+
+  render() {
+    // const match = React.PropTypes.object.isRequired
+    // const { params } = match;
+    // const activeExample = params.slug && examples.find(example => example.slug === params.slug);
+      return (
+        <div>
+          <div id="panel" className="collapsed">
+            <h1><a href="https://github.com/toxicFork/react-three-renderer/">react-three-renderer</a> / examples</h1>
+            <div id="content">
+              <div>
+                <h2>webgl</h2>
+                  <div>
+                  { this.props.isLoggedIntoSpotify ? (
+                    <div id="appBlock" className="flexcontainer-horizontal">
+                      <div className="col-md-4">
+                        <Corpus access={this.props.access_token} />
+                      </div>
+                      <div className="col-md-8">
+                        <Visualizer />
+                      </div>
+                    </div>
+                  ) : (
+                      <LoginSpotify />
+                  ) }
+                  </div>
+                {/* {examples.map((example, index) => {
+                  if (example.separator) {
+                    return (<h2 key={index}>{example.name}</h2>);
+                  }
+
+                  if (example.advanced) {
+                    return (<div key={index}>
+                      <a href={example.page} target="blank">{example.name}</a> (new tab)
+                    </div>);
+                  }
+
+                  return (<NavLink
+                    to={`/${example.slug}`}
+                    key={index}
+                    className="link"
+                    activeClassName="selected"
+                  >
+                    {example.name}
+                  </NavLink>);
+                })} */}
+              </div>
+            </div>
+          </div>
+          <Viewer example={activeExample} />
+        </div>
+      );
+    };
+
+  componentDidMount() {
+    const params = this.getHashParams(),
+      access_token = params.access_token,
+      state = params.state,
+      storedSpotifyState = window.localStorage['spotifyAuthKey']
+    if (!access_token && (state == null || state !== storedSpotifyState)) {
+      console.log('There was an error during Spotify authentication :(')
+    } else {
+      return this.props.saveToken(access_token)
+    }
+  }
+  getHashParams() {
+    let hashParams = {},
+      e,
+      regQuery = /([^&;=]+)=?([^&;]*)/g,
+      query = window.location.hash.substring(1)
+    while (e = regQuery.exec(query)) {
+      hashParams[e[1]] = decodeURIComponent(e[2])
+    }
+    return hashParams
+  }
+}
 
 Browser.propTypes = {
   match: React.PropTypes.object.isRequired,
 };
 
-export default Browser;
+// export default Browser;
+
+/* ----- IMPORT CONTAINER DEPENDENCIES ----- */
+
+import { connect } from 'react-redux'
+import { storeToken } from '../reducers'
+
+/* ----- CONTAINER ----- */
+
+const mapStateToProps = (store, ownProps) => {
+  return {
+    isLoggedIntoSpotify: store.isLoggedIntoSpotify,
+    access_token: store.access_token
+  }
+}
+
+const mapDispatchToProps = (dispatch, getState) => ({
+  saveToken: (token) => {
+    dispatch(storeToken(token))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,7 +1,106 @@
-import { combineReducers } from 'redux'
+// import { combineReducers } from 'redux'
 
-const rootReducer = combineReducers({
-  auth: require('./auth').default,
-})
+// const rootReducer = combineReducers({
+//   auth: require('./auth').default,
+// })
 
-export default rootReducer
+// export default rootReducer
+
+import axios from 'axios'
+
+const googleKey = process.env.GOOGLE_KEY.replace(/"/g, '') || require('../../secrets.js').googleKey
+
+/* -----------------    ACTIONS     ------------------ */
+
+const SET_CURRENT = 'SET_CURRENT'
+const SET_TOKEN = 'SET_TOKEN'
+const SET_CORPUS = 'SET_CORPUS'
+const SET_CHART_DATA = 'SET_CHART_DATA'
+
+
+/* ------------   ACTION CREATORS     ------------------ */
+
+const setCurr = (artist, song) => {
+  song = song.indexOf(' - ') > -1 ?
+    song.slice(0, song.indexOf(' - ')) :
+    song
+  return {
+    type: SET_CURRENT,
+    artist,
+    song
+  }
+}
+
+const setToken = token => ({ type: SET_TOKEN, token })
+
+const setCorpus = corpus => {
+  corpus = corpus.replace(/\n\n/g, '\n')
+    .replace(/\n/g, '.\n')
+  return {
+    type: SET_CORPUS,
+    corpus
+  }
+}
+
+// const parseSentences = arr => {
+//   return arr.map(obj => {
+//     return {
+//       x: obj.text.beginOffset,
+//       y: obj.sentiment.score,
+//       sentence: obj.text.content || 'No text'
+//     }
+//   })
+// }
+
+// const setChartData = serverResponse => {
+//   return {
+//     type: SET_CHART_DATA,
+//     data: {
+//       documentSentiment: serverResponse.documentSentiment,
+//       sentences: parseSentences(serverResponse.sentences)
+//     }
+//   }
+// }
+
+/* ------------       REDUCER     ------------------ */
+
+export default (state = {
+  data: {},
+  isLoading: false,
+  isLoggedIntoSpotify: false,
+  currSong: '',
+  currArtist: '',
+  corpus: '',
+  access_token: ''
+}, action) => {
+  const newState = Object.assign({}, state)
+  switch (action.type) {
+    case SET_CURRENT:
+      newState.currSong = action.song
+      newState.currArtist = action.artist
+      break
+
+    case SET_TOKEN:
+      newState.isLoggedIntoSpotify = true
+      newState.access_token = action.token
+      break
+
+    // case SET_CORPUS:
+    //   newState.corpus = action.corpus
+    //   break
+
+    // case SET_CHART_DATA:
+    //   newState.data = action.data
+    //   break
+
+    default:
+      return state
+  }
+  return newState
+}
+
+/* ------------       DISPATCHERS     ------------------ */
+
+export const storeToken = token => (dispatch) => {
+  return dispatch(setToken(token))
+}
