4a4507e62990b7786dbbba86aebf2083739abb48
Nancy <nevelasquez113@gmail.com>
2018-01-29 20:37:05 -0500

    Deleted localhost:1337 reference

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div>
         <Carousel
-            fixedItem={<a href="http://localhost:1337/webgl_planets" id="button-black" className="waves-effect waves-light btn">Open Your Mind</a>}
+            fixedItem={<a href="/webgl_planets" id="button-black" className="waves-effect waves-light btn">Open Your Mind</a>}
             options={{ fullWidth: true }}
             images={[
                 'https://images.unsplash.com/photo-1485795959911-ea5ebf41b6ae?dpr=1&auto=format&fit=crop&w=1080&h=711&q=80&cs=tinysrgb&crop=',
--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (!reasonableName.test(pkg.name)) {
 // and add it to the environment.
 // Note that this needs to be in your home directory, not the project's root directory
 const env = process.env
-    , secretsFile = resolve(require('homedir')(), `.${pkg.name}.env`)
+    , secretsFile = resolve(require('homedir')(), `.env`)
 
 try {
   Object.assign(env, require(secretsFile))
